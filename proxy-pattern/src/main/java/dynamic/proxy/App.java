5a59894fa1ab30dc9f7f6643e84577ac3c3583d6
Rovastus <rovastus1@gmail.com>
2020-02-23 15:55:59 +0100

    updated dynamic proxy example

--- proxy-pattern/src/main/java/dynamic/proxy/App.java
+++ proxy-pattern/src/main/java/dynamic/proxy/App.java
@@ -10,8 +10,30 @@ import proxy.VideoI;
 public class App {
     public static void main(String[] args) {
         VideoI proxyInstance = (VideoI) Proxy.newProxyInstance(VideoI.class.getClassLoader(),
-                new Class[] { VideoI.class }, new ObjectInvocationHandler(new Video("video")));
+                new Class[] { VideoI.class }, new ObjectInvocationHandler("video"));
 
+        System.out.println("--------");
+        proxyInstance.play();
+        proxyInstance.renameVideo("new_video");
+
+        System.out.println("--------");
+
+        proxyInstance = (VideoI) Proxy.newProxyInstance(VideoI.class.getClassLoader(), new Class[] { VideoI.class },
+                (proxy, method, methodArgs) -> {
+                    if (!(proxy instanceof Video)) {
+                        proxy = new Video("video");
+                    }
+
+                    System.out.println("Starting executing method " + method.getName());
+
+                    long start = System.nanoTime();
+                    Object result = method.invoke(proxy, methodArgs);
+                    long diff = System.nanoTime() - start;
+
+                    System.out.println("Method " + method.getName() + " finished in " + diff + " ns");
+
+                    return result;
+                });
         System.out.println("--------");
 
         proxyInstance.play();
@@ -20,18 +42,22 @@ public class App {
 }
 
 class ObjectInvocationHandler implements InvocationHandler {
-    private Object target;
+    private String videoName;
 
-    ObjectInvocationHandler(Object target) {
-        this.target = target;
+    ObjectInvocationHandler(String videoName) {
+        this.videoName = videoName;
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (!(proxy instanceof Video)) {
+            proxy = new Video(videoName);
+        }
+
         System.out.println("Starting executing method " + method.getName());
 
         long start = System.nanoTime();
-        Object result = method.invoke(target, args);
+        Object result = method.invoke(proxy, args);
         long diff = System.nanoTime() - start;
 
         System.out.println("Method " + method.getName() + " finished in " + diff + " ns");
